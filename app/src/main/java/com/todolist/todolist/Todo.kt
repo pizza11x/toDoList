c845d9c3483f3d4332c1dd3c100810c4c624c8fc
pizza11x <Cyrano1129>
2021-08-16 22:33:34 +0200

    Complete methods

--- app/src/main/java/com/todolist/todolist/Todo.kt
+++ app/src/main/java/com/todolist/todolist/Todo.kt
@@ -2,5 +2,5 @@ package com.todolist.todolist
 
 data class Todo(
     val title:String,
-    val isChecked:Boolean = false,
+    var isChecked:Boolean = false,
 )
--- app/src/main/java/com/todolist/todolist/TodoAdapter.kt
+++ app/src/main/java/com/todolist/todolist/TodoAdapter.kt
@@ -1,9 +1,10 @@
 package com.todolist.todolist
 
+import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_todo.view.*
 
@@ -23,11 +24,35 @@ class TodoAdapter(
         )
     }
 
+    fun addTodo(todo: Todo){
+        todos.add(todo)
+        notifyItemInserted(todos.size - 1)
+    }
+
+    fun deleteDoneTodos(){
+        todos.removeAll { todo ->
+            todo.isChecked
+        }
+        notifyDataSetChanged()
+    }
+
+    private fun toggleStrikeThrough(tvTodoTitle: TextView, isChecked: Boolean) {
+        if(isChecked) {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
+        } else {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+        }
+    }
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         val curTodo = todos[position]
         holder.itemView.apply {
             tvTodoTitle.text = curTodo.title
             cbDone.isChecked = curTodo.isChecked
+            toggleStrikeThrough(tvTodoTitle, curTodo.isChecked)
+            cbDone.setOnCheckedChangeListener { _, isChecked ->
+                toggleStrikeThrough(tvTodoTitle, isChecked)
+                curTodo.isChecked = !curTodo.isChecked
+            }
         }
     }
 
